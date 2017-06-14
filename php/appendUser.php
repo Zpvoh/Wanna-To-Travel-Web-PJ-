1de9506ddd7ffb63c8f9fb8be264c5107bba4385
Zpvoh <16302010023@fudan.edu.cn>
2017-06-14 14:25:06 +0800

    做完了Browse和分页

---
+++ php/appendUser.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: qyy
+ * Date: 2017/6/9
+ * Time: 16:52
+ */
+
+include_once "connectDatabase.php";
+
+$email=$_POST["email"];
+$password=$_POST["password"];
+$username=$_POST["username"];
+
+if($email=="" || $password=="" || $username=="" || strlen($password)<=6 || strlen($username)<3){
+    die("ERROR");
+}
+
+if(preg_match("/^[a-z|0-9]+@([a-z0-9]+\\.)+[a-z]{2,}$/i", $email)==0){
+    die("ERROR");
+}
+
+$rsUID=$db->query("select UID from traveluserdetails WHERE Email='{$email}'");
+
+if(!$rsUID->fetch_assoc()){
+    $db->query("insert into traveluser".
+                "(UserName, Pass, State, DateJoined, DateLastModified)".
+                "VALUES".
+                "('{$username}', '{$password}', 1, now(), now())");
+
+    $rs=$db->query("select UID from traveluser ORDER by UID DESC Limit 1");
+    $uid=$rs->fetch_assoc()["UID"];
+
+    $db->query("insert into traveluserdetails".
+                "(UID, Email, Privacy)".
+                "VALUES".
+                "({$uid},'{$email}', 1)");
+
+    echo $uid;
+}else{
+    echo "You've registered";
+}
